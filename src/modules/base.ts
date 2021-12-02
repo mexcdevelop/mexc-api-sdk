4a59f88549a07dd7543fd0e91c9b6761712d0e71
tsungilin <tsungilin@cloudwaretw.com>
2021-12-02 12:51:17 +0800

    新增request func & common api

--- src/modules/base.ts
+++ src/modules/base.ts
@@ -1,11 +1,50 @@
+import { buildQueryString, createRequest, removeEmptyValue } from '../util';
+import * as crypto from 'crypto';
+
 export class Base {
   public config: any = {};
-  public apiKey: any;
-  public apiSecret: any;
-  public baseURL: any;
   constructor(apiKey: any, apiSecret: any) {
     this.config.apiKey = apiKey;
     this.config.apiSecret = apiSecret;
-    this.config.baseURL = 'https://api.binance.com';
+    // this.config.baseURL = 'https://www.mxctest.com/open/api/v2';
+    this.config.baseURL = 'https://gatewayapi.mxctest.com/api/v3';
+  }
+
+  public publicRequest (method: any, path: any, paramsObj: any = {}): any {
+    paramsObj = removeEmptyValue(paramsObj)
+    paramsObj = buildQueryString(paramsObj)
+    if(paramsObj !== '') {
+      path = `${path}?${paramsObj}`
+    }
+
+    return createRequest({
+      method: method,
+      baseURL: this.config.baseURL,
+      url: path,
+      headers: {
+        'Content-Type': 'application/json',
+        'X-MBX-APIKEY': this.config.apiKey
+      }
+    })
+  }
+
+  public signRequest(method: any, path: any, paramsObj: any = {}): any {
+    const timestamp = Date.now()
+    paramsObj = removeEmptyValue(paramsObj)
+    const queryString = buildQueryString({ ...paramsObj, timestamp })
+    const signature = crypto
+        .createHmac('sha256', this.config.apiSecret)
+        .update(queryString)
+        .digest('hex')
+
+    return createRequest({
+      method: method,
+      baseURL: this.config.baseURL,
+      url: `${path}?${queryString}&signature=${signature}`,
+      headers: {
+        'Content-Type': 'application/json',
+        'X-MBX-APIKEY': this.config.apiKey
+      }
+    })
   }
 }
